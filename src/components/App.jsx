af111d22e5a94e0156a9a96a1ccb8635f806dcb0
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-05-22 22:45:09 +0300

    refactoring of App to Hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,19 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import ContactList from './ContactList/ContactList';
 import ContactForm from './ContactForm/ContactForm';
 import Filer from './Filter/Filter';
 
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export default function App() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  });
 
-  componentDidUpdate(_, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  formSubmitHandler = data => {
-    const preCheck = this.state.contacts.some(
+  const formSubmitHandler = data => {
+    const preCheck = contacts.some(
       ({ name, number }) =>
         name.toLowerCase() === data.name.toLowerCase() || number === data.number
     );
@@ -39,47 +27,40 @@ class App extends Component {
       ...data,
     };
 
-    this.setState(({ contacts }) => ({
-      contacts: [contact, ...contacts],
-    }));
+    setContacts([contact, ...contacts]);
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
   };
 
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
   };
 
-  getVisibleContacts = () => {
-    const { contacts, filter } = this.state;
+  const getVisibleContacts = () => {
     const normilizedFilter = filter.toLowerCase();
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normilizedFilter)
     );
   };
 
-  render() {
-    const { filter } = this.state;
-    const visibleContacts = this.getVisibleContacts();
-    return (
-      <div style={{ paddingLeft: 15 }}>
-        <h1 className="contact_title">Phonebook</h1>
-        <ContactForm onSubmit={this.formSubmitHandler} />
-        <h2 className="contact__title">Contacts</h2>
-        <Filer value={filter} onChange={this.changeFilter} />
-        {visibleContacts.length > 0 && (
-          <ContactList
-            visibleContacts={visibleContacts}
-            onDeleteContact={this.deleteContact}
-          />
-        )}
-      </div>
-    );
-  }
-}
+  const visibleContacts = getVisibleContacts();
 
-export default App;
+  return (
+    <div style={{ paddingLeft: 15 }}>
+      <h1 className="contact_title">Phonebook</h1>
+      <ContactForm onSubmit={formSubmitHandler} />
+      <h2 className="contact__title">Contacts</h2>
+      <Filer value={filter} onChange={changeFilter} />
+      {visibleContacts.length > 0 && (
+        <ContactList
+          visibleContacts={visibleContacts}
+          onDeleteContact={deleteContact}
+        />
+      )}
+    </div>
+  );
+}
